88bf87c83ee2d28a50bd5c39a77ce5cd26f2e2c1
vtka <slavik.tkachenko@gmail.com>
2017-08-09 21:15:56 +0600

    complete right_tangular

---
+++ lesson1/right_tangular.rb
@@ -0,0 +1,45 @@
+puts "Сторона А(см) "
+a = Integer(gets.chomp) 
+puts "Сторона B(см) "
+b = Integer(gets.chomp) 
+puts "Сторона C(см) "
+c = Integer(gets.chomp) 
+hypo = a 
+hypo = b if b > a 
+hypo = c if c > b && c > a
+if hypo == a 
+	cath_1 = b
+	cath_2 = c
+elsif hypo == c
+	cath_1 = a
+	cath_2 = b
+else 
+	cath_1 = c
+	cath_2 = a
+end	
+is_rightangular = hypo**2 == cath_1**2 + cath_2**2 
+is_rightsided = a == b || b == c || a == c 
+is_equalsided = a == b && b == c && c == a 
+if is_rightangular 
+	puts "Этот треугольник прямоугольный. Квадрат гипотенузы (#{hypo**2} см) равен сумме квадрата катетов (#{cath_1**2+cath_2**2} см)."
+else
+	puts "Этот треугольник не прямоугольный."
+end
+if is_rightsided
+	puts "Этот треугольник является равнобедренным."
+else
+	puts "Этот треугольник не является равнобедренным."
+end
+if is_equalsided
+	puts "Этот треугольник является равносторонним."
+else 
+	puts "Этот треугольник не является равносторонним."
+end
+
+
+
+
+
+
+
+
