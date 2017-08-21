2414fa6878795c7f30ccb028224edebc3e8dbcb0
vtka <slavik.tkachenko@gmail.com>
2017-08-21 20:43:42 +0600

    update lesson2 assignment 4

--- lesson2/alphabet.rb
+++ lesson2/alphabet.rb
@@ -1,5 +1,5 @@
 hash = {}
 ("a".."z").to_a.each_with_index do |letter, index|
-  hash[letter] = index + 1
+  hash[letter] = index + 1 if [ "a", "e", "i", "o", "u" ].include?(letter)
 end
-print hash
+print hash 
