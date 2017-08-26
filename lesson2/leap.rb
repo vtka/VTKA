5c62f6890b020dd487041aecf6ebd5efe8e123eb
vtka <slavik.tkachenko@gmail.com>
2017-08-26 17:14:28 +0600

    add leap.rb

--- lesson2/leap.rb
+++ lesson2/leap.rb
@@ -4,4 +4,21 @@ day = gets.chomp.to_i
 print "Month (numerical) "
 month = gets.chomp.to_i
 print "Year (A.D) "
-month = gets.chomp.to_i
+year = gets.chomp.to_i
+
+count = day
+i = 0
+
+days_month_count = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+if year%4 == 0 && year%100 !=0 || year%400 == 0
+  days_month_count[1] = 29
+end
+
+while i < month - 1
+  count = count + days_month_count[i]
+  i = i + 1
+  puts count
+end
+
+puts "The index of this date is #{count}"
