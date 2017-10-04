e7f6d706f7beca72a119b8e6a7d88e21c3d027ef
vtka <slavik.tkachenko@gmail.com>
2017-10-04 23:21:47 +0600

    fix bug

--- lesson1_new/quad_equation.rb
+++ lesson1_new/quad_equation.rb
@@ -14,13 +14,13 @@ puts "Коэффициент c:"
 c = gets.chomp.to_f
 
 d = b**2 - 4*a*c
-d_root = Math.sqrt(d.abs)
 
 if d < 0
   puts "Корней нет!"
 elsif d == 0
   puts "Дискриминант равен #{d}. Корень равен 0!"
 else
+  d_root = Math.sqrt(d.abs)
   x1 = +(-b + d_root) / 2*a
   x2 = +(-b - d_root) / 2*a
   puts "Дискриминант равен #{d}, корень X1 равен #{x1}, корень X2 равен #{x2}"
