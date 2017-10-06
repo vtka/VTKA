5d493beebb0fd469e333ff70a7eb45a4e01cf29f
vtka <slavik.tkachenko@gmail.com>
2017-10-06 21:00:16 +0600

    add first 4 assignments

---
+++ lesson2_new/10_to_100.rb
@@ -0,0 +1,15 @@
+# Заполнить массив числами от 10 до 100 с шагом 5
+#Syntax
+#while conditional [do]
+#   code
+#end
+
+a=[]
+i =10
+
+while i <= 100 do 
+  a << i
+  i = i+5
+end
+
+print a
---
+++ lesson2_new/months.rb
@@ -0,0 +1,21 @@
+# Сделать хеш, содеращий месяцы и количество дней в месяце. 
+# В цикле выводить те месяцы, у которых количество дней ровно 30
+
+months = {
+"January" => 31, 
+"February" => 29,
+"March" => 31, 
+"April" => 30, 
+"May" => 31, 
+"June" => 30, 
+"July" => 31,
+"August" => 31, 
+"September" => 30, 
+"October" => 31, 
+"November" => 30, 
+"December" => 31 
+}
+
+months.each do |k, v|
+  puts k if v == 30
+end
